4e85624ae400d99ae2da04f69c01e928a13559be
prog-RAM-zing <vignesh.athreya@gmail.com>
2014-12-01 18:01:37 +0530

    First Commit. Complete code for up-to-date scrapper and mapper

---
+++ geocoding.py
@@ -0,0 +1,31 @@
+__author__ = 'vigneshathreya'
+
+
+import urllib2
+import json
+import db_class_general, sqlite3
+
+def get_latlng(add,type=''):
+    DB = db_class_general.DBManager(sqlite3.connect('News_Db.db'))
+    res = DB.other_sqlite_ops('select * from Geocode where Place="%s" COLLATE NOCASE'%(add))
+    if not res:
+        address = urllib2.quote(add)
+        geocode_url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % address
+        print geocode_url
+        req = urllib2.urlopen(geocode_url)
+        jsonResponse = json.loads(req.read())
+        if jsonResponse['status'] == 'ZERO_RESULTS' or not jsonResponse['results']:
+            return False
+        else:
+            latlng = (jsonResponse['results'][0]['geometry']['location']['lat'],jsonResponse['results'][0]['geometry']['location']['lng'])
+            try:
+                DB.insert_record('Geocode',Place = add, LatLng = ','.join([str(z) for z in latlng]),Place_Type = type)
+            except sqlite3.IntegrityError:
+                pass
+            return latlng
+
+    else:
+        return tuple([float(z) for z in res[0][1].split(',')])
+
+
+
